969f624fef1bdf04513f827abbaa0340de2246ee
Ahmed Zaher <82454482+JadeZaher@users.noreply.github.com>
2024-07-21 14:51:23 -0600

    fix book button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
 		return new Promise((resolve) => {
 			document.startViewTransition(async () => {
 				resolve();
+				bookToggle = false;
 				await navigation.complete;
 			});
 		});
@@ -44,7 +45,7 @@
 		</a>
 		<button on:click={(e) => toggleBookingForm(e)} class="btn btn-outline btn-secondary md:mx-2 ml-[2px]"> Book </button>
 	</div>
-	<form on:submit={toggleState} style="opacity: {bookToggle? 0 : 1}; transform: {bookToggle? "scale(0)" : "scale(1)"}" 
+	<form on:submit={toggleState} style="opacity: {bookToggle? 1 : 0}; transform: {bookToggle? "scale(1)" : "scale(0)"}" 
 		class="duration-300 z-30 bg-black border-accent border-2 p-6 rounded-md flex justify-between flex-col h-[70vh]  md:w-[50vw] w-full absolute bottom-[16%] md:left-2 " 
 		action="https://public.herotofu.com/v1/85b123e0-4796-11ef-9c40-81ddfd46a512" 
 		method="post" 
